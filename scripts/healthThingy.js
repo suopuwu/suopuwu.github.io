71460453c933e29491f3e72cdba8eb4e6ad6e72b
uL-hpesoJ <Josephlu8@gmail.com>
2017-01-16 21:48:51 -0800

    V0.22
    +Some stuff for a school project
    +Usage of JSON data to build page

---
+++ scripts/healthThingy.js
@@ -0,0 +1,31 @@
+var $, window;
+var currentWindow = 0;
+$(function () {
+    "use strict";
+    console.log('succ');
+    var data = $.getJSON('json/tenReasons.json', (function () {
+        data = data.responseJSON;
+        $.each(data.reasons, function (index, val) {
+            $('<div id="' + index + '" class="container" style=" background: ' + val.bg + '; flex-direction: column; justify-content: flex-start; align-items: center;"><h1 id="event" style="transform: scale(1); font-size: 2.5em;">' + val.event + '</h1><h2 id="date">' + val.date + '</h2><div id="spacer" style="height: ' + (window.innerHeight / 2.3) + 'px"></div><div style="padding: 50px"><h3 id="explanation">' + val.reason + '</h3></div></div>').appendTo('body');
+        });
+
+        $('<li>' + data.reasons[0].event + '</li>').appendTo('ul');
+    }));
+    $(window).on('wheel', function (e) {
+        var scrollNum = e.originalEvent.deltaY;
+        if (scrollNum < 0 && currentWindow > 0) { //on scroll up
+            currentWindow -= 1;
+            scrollTo('#' + currentWindow);
+
+        } else if (scrollNum > 0 && currentWindow !== data.reasons.length - 1) { //on scroll down
+            currentWindow += 1;
+            scrollTo('#' + currentWindow);
+        }
+    });
+
+    function scrollTo(targetElement) {
+        $('html, body').animate({
+            scrollTop: $(targetElement).offset().top
+        }, 500);
+    }
+});
