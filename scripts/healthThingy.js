62da83b8ee8eb764e6d883223bc1aafbc939e0cb
uL-hpesoJ <Josephlu8@gmail.com>
2017-01-17 08:14:58 -0700

    Update healthThingy.js

--- scripts/healthThingy.js
+++ scripts/healthThingy.js
@@ -6,7 +6,7 @@ $(function () {
     var data = $.getJSON('json/tenReasons.json', (function () {
         data = data.responseJSON;
         $.each(data.reasons, function (index, val) {
-            $('<div id="' + index + '" class="container" style=" background: ' + val.bg + '; flex-direction: column; justify-content: flex-start; align-items: center;"><h1 id="event" style="transform: scale(1); font-size: 2.5em;">' + val.event + '</h1><h2 id="date">' + val.date + '</h2><div id="spacer" style="height: ' + (window.innerHeight / 2.3) + 'px"></div><div style="padding: 50px"><h3 id="explanation">' + val.reason + '</h3></div></div>').appendTo('body');
+            $('<div id="' + index + '" class="container" style=" background: ' + val.bg + '; flex-direction: column; justify-content: flex-start; align-items: center;"><h1 id="event" style="transform: scale(1); font-size: 2.5em;">' + val.event + '</h1><h2 id="date">' + val.date + '</h2><div id="spacer" style="height: ' + (window.innerHeight / 3) + 'px"></div><div style="padding: 50px"><h3 id="explanation">' + val.reason + '</h3></div></div>').appendTo('body');
         });
 
         $('<li>' + data.reasons[0].event + '</li>').appendTo('ul');
